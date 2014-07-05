a7e78957ce28f9e0d31794206bfec06d593b960c
JPen3 <jerry_pendleton@yahoo.com>
2014-07-05 18:30:21 -0500

    Folders for Mothman
    Initial folders for Mothman.

---
+++ Mothman/src/spriteObject.js
@@ -0,0 +1,49 @@
+var spriteObject =
+{
+ 	SIZEX: 0,
+  	SIZEY: 0,
+  	ROWS: 0,
+  	COLUMNS: 0,
+    
+  	sourceX: 0,
+  	sourceY: 0,
+  	sourceWidth: 0,
+  	sourceHeight: 0,
+  	width: 0,
+  	height: 0,
+  	x: 0,
+  	y: 0,
+  	vx: 0,
+  	vy: 0,
+  	visible: true,
+  	rotation: 0,
+
+	centerX: 0,
+	centerY: 0,
+	halfWidth: 0,
+	halfHeight: 0,
+	
+	spriteType: 0,
+	STATE: 0,
+      
+  	//Getters
+  	getCenterX: function()
+  	{
+    	return this.centerX;
+  	},
+	
+  	getCenterY: function()
+  	{
+    	return this.centerY;
+  	},
+	
+  	getHalfWidth: function()
+  	{
+    	return this.halfWidth;
+  	},
+	
+  	getHalfHeight: function()
+  	{
+    	return this.halfHeight;
+  	}	
+};
